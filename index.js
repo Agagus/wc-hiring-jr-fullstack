8a50b382aefa23d0249c66707af333394e16227f
Agustina Zanetti <agus.zanetti4@gmail.com>
2023-02-14 17:30:39 -0300

    resolved the 2 problems

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ However, if you have any questions, please send us an email
 to support@webcat.app with the subject "Jr Fullstack Test Questions"
 */
 
-import $t from './libs/test.js'
+import $t from './libs/test.js';
 
 /*
 1. Data manipulation:
@@ -33,12 +33,47 @@ import $t from './libs/test.js'
   Hint: Use native array methods as well as
     Lodash(https://lodash.com/docs) modules.
 */
-import _ from 'lodash'
-const source = $t.source(1)
+import _ from 'lodash';
+const source = $t.source(1);
 $t.answer(1, async () => {
   // Your code goes here
-  return 
-})
+  let targetData = {
+    balance: 0, //= income - expenses
+    income: 0,
+    expenses: 0,
+    byCategories: {
+      Restaurants: 0,
+      Income: 0,
+      Groceries: 0,
+      Rent: 0,
+    },
+  };
+
+  source.map((elem) => {
+    if (elem.category === 'Restaurants') {
+      targetData.byCategories.Restaurants -= elem.amount;
+      targetData.expenses += elem.amount;
+      targetData.balance -= elem.amount;
+    }
+    if (elem.category === 'Income') {
+      targetData.byCategories.Income += elem.amount;
+      targetData.income += elem.amount;
+      targetData.balance += elem.amount;
+    }
+    if (elem.category === 'Groceries') {
+      targetData.byCategories.Groceries -= elem.amount;
+      targetData.expenses += elem.amount;
+      targetData.balance -= elem.amount;
+    }
+    if (elem.category === 'Rent') {
+      targetData.byCategories.Rent -= elem.amount;
+      targetData.expenses += elem.amount;
+      targetData.balance -= elem.amount;
+    }
+  });
+
+  return targetData;
+});
 
 /*
 2. Asynchronous programming: 
@@ -47,11 +82,19 @@ $t.answer(1, async () => {
   3. Finally, return the list of resulting texts as an array.
     
 */
-const $source = $t.source(2)
+const $source = $t.source(2);
 $t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
-})
+  // Your code goes here:
+  // 1. Get ids: $source.getIds()
+  // 2. Get text for every id: $source.getText(id)
+  // 3. Return array of texts
+  let ids = await $source.getIds();
+
+  let array = [];
+
+  for (let i = 0; i < ids.length; i++) {
+    array.push(await $source.getText(ids[i]));
+  }
+
+  return array;
+});
